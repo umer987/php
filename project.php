b9230de82426518816415eb6ae7dbc8ffb4a1f15
syed umer <umershakir987@gmail.com>
2026-09-11 15:22:08 +0500

    user input

---
+++ project.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = readline("please enter your name");
+$age = readline("please enter your age");
+$father = readline("enter yor father name");
+
+echo "your name is " , $name;
+echo "your age is " , $age;
+echo "your father name is " , $father;
+
+
+
+?>
