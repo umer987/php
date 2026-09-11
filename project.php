1c121a64330899c29ea8a22322eba479709d9194
syed umer <umershakir987@gmail.com>
2026-09-11 15:22:59 +0500

    user input

--- project.php
+++ project.php
@@ -4,9 +4,9 @@ $name = readline("please enter your name");
 $age = readline("please enter your age");
 $father = readline("enter yor father name");
 
-echo "your name is " , $name;
-echo "your age is " , $age;
-echo "your father name is " , $father;
+echo "your name is " , $name ,  PHP_EOL;
+echo "your age is " , $age,  PHP_EOL;
+echo "your father name is " , $father,  PHP_EOL;
 
 
 
