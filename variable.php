bc2fe2ed4ced067a59eab8ea590559257a9372ff
syed umer <umershakir987@gmail.com>
2026-09-23 00:16:04 +0500

    adding git hub code

--- variable.php
+++ variable.php
@@ -22,6 +22,7 @@ Route::get('/user/{id}', function ($id) {
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
 
 ?>
