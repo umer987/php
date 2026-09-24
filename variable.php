8f81cf8f94e7c5b4575192c9cece195a3fc1de9c
syed umer <umershakir987@gmail.com>
2026-09-24 19:32:34 +0500

    ok

--- variable.php
+++ variable.php
@@ -23,7 +23,6 @@ Route::get('/user/{id}', function ($id) {
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-Route::get('/user/{id}', [PageController::class, 'user'])->name('user');
 
 
 
