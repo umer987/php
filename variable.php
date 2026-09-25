8e408ceee7125ff11d1d4f10ca6bde39fad5e84b
syed umer <umershakir987@gmail.com>
2026-09-26 00:05:20 +0500

    adding backend code

--- variable.php
+++ variable.php
@@ -17,11 +17,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+
 
 
 
