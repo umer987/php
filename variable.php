792f9facae0256bfec45b328f524f81f5c7d0f2a
syed umer <umershakir987@gmail.com>
2026-09-26 00:06:29 +0500

    adding backend code

--- variable.php
+++ variable.php
@@ -11,7 +11,6 @@ curl http://127.0.0.1:8000/greet/John      # GET with param
 curl -X POST http://127.0.0.1:8000/posts   # POST
 curl -X PUT http://127.0.0.1:8000/posts/1  # PUT
 curl -X DELETE http://127.0.0.1:8000/posts/1
-curl -L http://127.0.0.1:8000/old-page      # Follow redirect
 
 
 
