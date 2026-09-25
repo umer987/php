da59f58799ec8b1f1f5c3b9be0bdac0b08d98bd1
syed umer <umershakir987@gmail.com>
2026-09-26 00:06:18 +0500

    adding backend code

--- variable.php
+++ variable.php
@@ -6,7 +6,13 @@ echo'my name and age is ',$name ," ", $age;
 
 
 
-
+curl http://127.0.0.1:8000/                # GET
+curl http://127.0.0.1:8000/greet/John      # GET with param
+curl -X POST http://127.0.0.1:8000/posts   # POST
+curl -X PUT http://127.0.0.1:8000/posts/1  # PUT
+curl -X DELETE http://127.0.0.1:8000/posts/1
+curl -L http://127.0.0.1:8000/old-page      # Follow redirect
+curl http://127.0.0.1:8000/unknown          # 404 fallback
 
 
 
